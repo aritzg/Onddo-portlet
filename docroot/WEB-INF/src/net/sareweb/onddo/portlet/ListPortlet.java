1de4d3aaff43fe1046718095cedb0ef453fbdb83
Aritz Galdos <galdos.aritz@gmail.com>
2012-04-10 17:06:57 +0200

    Created Onddo! Again

---
+++ docroot/WEB-INF/src/net/sareweb/onddo/portlet/ListPortlet.java
@@ -0,0 +1,11 @@
+package net.sareweb.onddo.portlet;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class ListPortlet
+ */
+public class ListPortlet extends MVCPortlet {
+ 
+
+}
